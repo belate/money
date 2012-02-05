ed1ff87a3815c504ab3e1495a41fa7e76e71ec37
Belate Atorrasagasti <belateatorra@gmail.com>
2012-02-05 21:04:22 +0100

    branch bootstrap

--- settings.py
+++ settings.py
@@ -101,6 +101,15 @@ MIDDLEWARE_CLASSES = (
 	'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+)
+
 ROOT_URLCONF = 'money.urls'
 
 TEMPLATE_DIRS = (
