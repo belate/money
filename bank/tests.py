55b53589738b8828df336fedf79ad83485a2afd4
Belate Atorrasagasti <belateatorra@gmail.com>
2012-07-10 22:36:19 +0100

    fist test

--- bank/tests.py
+++ bank/tests.py
@@ -6,11 +6,22 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        self.user = User.objects.create_user(username="belate", email="belate@example.com", password="belate")
+
+    def test_index(self):
+
+        response = self.client.get("/")
+        self.assertRedirects(response,"/accounts/login/?next=/")
+
+        self.client.login(username="belate", password="belate")
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response,"bank/index.html")
+
+
